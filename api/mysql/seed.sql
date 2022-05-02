3f2845eed6fe16e0d1f39555ea567c06c3e99073
ethan-sch <ethan@unit203.com>
2022-05-02 13:15:16 -0700

    fix/sql-seed-file

--- api/mysql/seed.sql
+++ api/mysql/seed.sql
@@ -1,5 +1,3 @@
 USE code_test;
 
-bib, name, time (hh:mm:ss)
-123, john doe, 01:02:03
-321, jane doe, 01:01:01
+INSERT INTO `results` (bib, name, time) VALUES (123, john doe, 01:02:03), (321, jane doe, 01:01:01);
